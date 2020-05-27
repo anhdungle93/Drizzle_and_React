1b78f80460c5e4895a4eb083b7878e1fc9dac513
anhdungle <anhdungle634@gmail.com>
2020-05-27 23:25:37 +0200

    instantiate the Drizzle store

--- client/src/index.js
+++ client/src/index.js
@@ -4,10 +4,23 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import {Drizzle, generateStore } from "@drizzle/store";
+import MyStringStore from "./contracts/MyStringStore.json";
+
+const options = {
+  contracts: [MyStringStore],
+  web3: {
+    fallback: {
+      type: "ws",
+      url: "ws://127.0.0.1:9545",
+    },
+  },
+};
+
+const drizzle = new Drizzle(options);
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App drizzle={drizzle} />
+,
   document.getElementById('root')
 );
 
